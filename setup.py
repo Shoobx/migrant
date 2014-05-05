0685f0134279ec97b45c2cc72635fa6c2ffcbc78
Andrey Lebedev <andrey@lebedev.lt>
2014-05-05 13:54:48 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="migrant",
-    version='0.1.0dev',
+    version='0.5.0dev',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Database Migration Engine",
