ac3ada01fd91181dcf4abb9e67a602a716e3fe0f
Adam Groszer <agroszer@gmail.com>
2017-03-20 15:02:17 +0100

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="migrant",
-    version='0.7.4dev',
+    version='0.7.5dev',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Database Migration Engine",
